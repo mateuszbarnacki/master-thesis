ca017f8b919cece70d0663ae16cd62534901e2c0
mateuszbarnacki <barnackimateusz@gmail.com>
2023-03-08 13:10:12 +0100

    Add user mapper

---
+++ auth-service/src/main/java/agh/wfiis/weather/service/UserMapper.java
@@ -0,0 +1,42 @@
+package agh.wfiis.weather.service;
+
+import agh.wfiis.weather.common.UserRole;
+import agh.wfiis.weather.dto.UserDto;
+import agh.wfiis.weather.exception.RoleNotFoundException;
+import agh.wfiis.weather.model.RoleEntity;
+import agh.wfiis.weather.model.UserEntity;
+import agh.wfiis.weather.repository.RoleRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+class UserMapper {
+    private final RoleRepository roleRepository;
+
+    public UserMapper(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    UserEntity mapDtoToEntity(UserDto userDto) {
+        UserEntity entity = new UserEntity();
+
+        entity.setUsername(userDto.username());
+        entity.setPassword(userDto.password());
+        entity.addRoles(mapUserRolesToRoleEntities(userDto.roles()));
+
+        return entity;
+    }
+
+    private Set<RoleEntity> mapUserRolesToRoleEntities(Set<UserRole> roles) {
+        Set<RoleEntity> roleEntities = new HashSet<>();
+        roles.forEach(userRole -> roleEntities.add(mapUserRoleToRoleEntity(userRole)));
+        return roleEntities;
+    }
+
+    private RoleEntity mapUserRoleToRoleEntity(UserRole userRole) {
+        return roleRepository.findByName(userRole.getRole())
+                .orElseThrow(() -> new RoleNotFoundException(userRole.getRole()));
+    }
+}
