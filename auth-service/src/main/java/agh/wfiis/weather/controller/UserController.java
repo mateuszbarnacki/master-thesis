0ca694a328f035045aa935441a79be11974c37cd
mateuszbarnacki <barnackimateusz@gmail.com>
2023-03-08 13:11:56 +0100

    Add user controller

---
+++ auth-service/src/main/java/agh/wfiis/weather/controller/UserController.java
@@ -0,0 +1,27 @@
+package agh.wfiis.weather.controller;
+
+import agh.wfiis.weather.dto.UserDto;
+import agh.wfiis.weather.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public void registerUser(@RequestBody UserDto userDto) {
+        userService.registerUser(userDto);
+    }
+}
---
+++ auth-service/src/test/java/agh/wfiis/weather/controller/UserControllerTest.java
@@ -0,0 +1,63 @@
+package agh.wfiis.weather.controller;
+
+import agh.wfiis.weather.common.UserRole;
+import agh.wfiis.weather.dto.UserDto;
+import agh.wfiis.weather.service.RestUserService;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Set;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+
+@WebMvcTest({UserController.class})
+class UserControllerTest {
+    private static ObjectMapper objectMapper;
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private RestUserService userService;
+
+    @BeforeAll
+    public static void setUp() {
+        objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+    }
+
+    @Test
+    @WithMockUser
+    void shouldRegisterUser() throws Exception {
+        Object user = new Object() {
+            private final String username = "Test";
+            private final String password = "Test12345";
+            private final Set<UserRole> roles = Set.of(UserRole.ADMIN);
+        };
+        String json = objectMapper.writeValueAsString(user);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/user")
+                .with(csrf())
+                .characterEncoding(StandardCharsets.UTF_8)
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON_VALUE);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+
+        verify(userService).registerUser(any(UserDto.class));
+    }
+}
