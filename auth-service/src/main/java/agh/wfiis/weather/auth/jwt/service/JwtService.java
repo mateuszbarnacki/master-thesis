479dcc2b237d7ca8062de7f65582dbafbca90ec3
mateuszbarnacki <barnackimateusz@gmail.com>
2023-03-14 22:07:40 +0100

    Add jwt service

---
+++ auth-service/src/main/java/agh/wfiis/weather/auth/jwt/service/JwtService.java
@@ -0,0 +1,8 @@
+package agh.wfiis.weather.auth.jwt.service;
+
+import org.springframework.security.core.Authentication;
+
+public interface JwtService {
+    String generateToken(Authentication authentication);
+    boolean checkToken(String token);
+}
