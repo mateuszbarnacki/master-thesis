e4ffbc9d6c9f374637d9d67f9217c55e9b21eeea
mateuszbarnacki <barnackimateusz@gmail.com>
2023-05-06 14:10:48 +0200

    Remove get controller

--- measurement-post-service/controllers/measurements.js
+++ measurement-post-service/controllers/measurements.js
@@ -1,16 +1,6 @@
 const axios = require('axios');
 const connection = require('../db/connection');
 
-const getMeasurements = async (req, res) => {
-    try {
-        const db = await connection.getConnection();
-        const result = await db.collection(req.params.project).find().toArray();
-        res.status(200).send(result);
-    } catch (error) {
-        res.status(500).send({message: error.message});
-    }
-};
-
 const addMeasurement = async (req, res) => {
     try {
         const {data:project} = await axios.get(`http://localhost:13401/projects/${req.params.project}`);
@@ -27,6 +17,5 @@ const addMeasurement = async (req, res) => {
 };
 
 module.exports = {
-    getMeasurements,
     addMeasurement
 };
--- measurement-post-service/routes/measurements.js
+++ measurement-post-service/routes/measurements.js
@@ -1,8 +1,6 @@
 const express = require('express');
-const {getMeasurements, addMeasurement} = require('../controllers/measurements');
+const {addMeasurement} = require('../controllers/measurements');
 const router = express.Router();
-
-router.get('/:project', getMeasurements);
 
 router.post('/:project/:deviceId', addMeasurement);
 
