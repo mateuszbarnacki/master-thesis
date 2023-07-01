0ae4d4faae5100a7e3c204419a698c92c18b3b45
mateuszbarnacki <barnackimateusz@gmail.com>
2023-07-01 23:13:07 +0200

    Add filter list functionality

--- client/src/components/project/ProjectList.js
+++ client/src/components/project/ProjectList.js
@@ -1,11 +1,12 @@
 import Form from "react-bootstrap/Form";
 import ListGroup from "react-bootstrap/ListGroup";
 import ListGroupItem from "react-bootstrap/ListGroupItem";
-import {Fragment} from "react";
+import {Fragment, useState} from "react";
 import Card from "react-bootstrap/Card";
 import CardHeader from "react-bootstrap/CardHeader";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 import Button from "react-bootstrap/Button";
+import {isStringNullOrEmpty} from "./addProject/FormValidator";
 
 const mock = [
     'proj1',
@@ -16,21 +17,21 @@ const mock = [
     'pomiar',
     'pomiar testowy',
     'pomiar2',
-    'poject',
-    'poject',
-    'poject',
-    'poject',
-    'poject',
-    'poject',
-    'poject',
-    'poject',
-    'poject',
-    'poject',
-    'poject',
-    'poject',
+    'project'
 ];
 
 function ProjectList() {
+    const [list, setList] = useState(mock);
+    const handleOnChange = (event) => {
+        const searchValue = event.target.value;
+        console.log(searchValue);
+        if (isStringNullOrEmpty(searchValue)) {
+            setList(mock);
+        } else {
+            const newList = mock.slice().filter(value => value.match(searchValue + ".*"));
+            setList(newList);
+        }
+    };
     const displayCard = (item) => {
         return (
             <Card>
@@ -47,27 +48,22 @@ function ProjectList() {
                 </Card.Body>
             </Card>
         )
-    }
+    };
 
     return (
         <Fragment>
             <Form.Label htmlFor="search">Wyszukaj projekt po nazwie:</Form.Label>
-            <Form.Control
-                type="text"
-                id="search"
-                placeholder="Nazwa projektu"
-            />
+            <Form.Control type="text" id="search" placeholder="Nazwa projektu" onChange={(e) => handleOnChange(e)}/>
             <ListGroup variant="flush"
                        style={{
                            border: "1px solid black",
                            height: "60vh",
                            overflowY: "auto",
-                           marginTop: "2vh"
-                       }}>
-                {mock.map(item =>
+                           marginTop: "2vh"}}>
+                {list.map(item =>
                     <ListGroupItem key={item}
                                    action
-                                   onClick={displayCard}>
+                                   onClick={() => displayCard(item)}>
                         {item}
                     </ListGroupItem>)}
             </ListGroup>
