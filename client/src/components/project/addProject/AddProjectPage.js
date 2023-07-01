6f8be0dbc6ccf74daae58e722305b27ab3a6c01b
mateuszbarnacki <barnackimateusz@gmail.com>
2023-07-01 21:12:17 +0200

    Add handlers for add and close buttons

--- client/src/components/project/addProject/AddProjectPage.js
+++ client/src/components/project/addProject/AddProjectPage.js
@@ -1,14 +1,29 @@
 import Card from "react-bootstrap/Card";
-import {Fragment} from "react";
+import {Fragment, useState} from "react";
 import Button from "react-bootstrap/Button";
 import SensorForm from "./SensorForm";
 import Menu from "../../Menu";
 import ProjectForm from "./ProjectForm";
+import CardHeader from "react-bootstrap/CardHeader";
 
 function AddProjectPage() {
-    function handleClick(event) {
-
-    }
+    const [ids, setIds] = useState(Array.of(0));
+    const [counter, setCounter] = useState(1);
+    const handleAddSensorClick = () => {
+        const newIds = ids.slice();
+        newIds.push(counter);
+        setIds(newIds);
+        setCounter(counter+1);
+    };
+    const handleRemoveSensorClick = (id) => {
+        const newIds = ids.slice();
+        newIds.splice(id, 1);
+        if (newIds.length === 0) {
+            newIds.push(counter);
+            setCounter(counter+1);
+        }
+        setIds(newIds);
+    };
 
     return (
         <Fragment>
@@ -18,7 +33,16 @@ function AddProjectPage() {
                 <Card.Body>
                     <Card.Text as="h4">Dodaj nowy projekt</Card.Text>
                     <ProjectForm/>
-                    <SensorForm/>
+                    <Card className="border-black m-3 bg-light-subtle">
+                        <CardHeader>Etap 2. Uzupełnij informacje o czujnikach</CardHeader>
+                        <Card.Body>
+                            {ids.map((sensorId, index) => <SensorForm id={index}
+                                                             key={"sensor-" + sensorId}
+                                                             onAddSensorClick={() => handleAddSensorClick()}
+                                                             onRemoveSensorClick={() => handleRemoveSensorClick(index)}
+                                                             isAddButtonVisible={index === ids.length-1}/>)}
+                        </Card.Body>
+                    </Card>
                     <Button variant="dark" className="me-3 rounded-5 float-end">Zapisz projekt</Button>
                 </Card.Body>
             </Card>
--- client/src/components/project/addProject/SensorForm.js
+++ client/src/components/project/addProject/SensorForm.js
@@ -1,5 +1,3 @@
-import CardHeader from "react-bootstrap/CardHeader";
-import Card from "react-bootstrap/Card";
 import CloseButton from "react-bootstrap/CloseButton";
 import Form from "react-bootstrap/Form";
 import FormLabel from "react-bootstrap/FormLabel";
@@ -11,7 +9,7 @@ import Button from "react-bootstrap/Button";
 import {useState} from "react";
 import {isNumberOutOfRange, isStringNullOrEmpty} from "./FormValidator";
 
-function SensorForm() {
+function SensorForm({id, onAddSensorClick, onRemoveSensorClick, isAddButtonVisible}) {
     const [isSensorIdInvalid, setIsSensorIdInvalid] = useState(false);
     const [isLongitudeInvalid, setIsLongitudeInvalid] = useState(false);
     const [isLatitudeInvalid, setIsLatitudeInvalid] = useState(false);
@@ -29,66 +27,63 @@ function SensorForm() {
     }
 
     return (
-        <Card className="border-black m-3 bg-light-subtle">
-            <CardHeader>Etap 2. Uzupełnij informacje o czujnikach</CardHeader>
-            <Card.Body>
-                <Form className="m-1 bg-light-subtle">
-                    <FormLabel as="h4" className="m-1">
-                        Czujnik
-                        <CloseButton className="float-end"/>
-                    </FormLabel>
-                    <FormGroup className="mt-3">
-                        <FormLabel htmlFor="sensorId">ID urządzenia pomiarowego:</FormLabel>
-                        <FormControl required
-                                     id="sensorId"
-                                     type="text"
-                                     placeholder="Identyfikator urządzenia pomiarowego"
-                                     onChange={handleOnChangeSensorId}
-                                     isInvalid={isSensorIdInvalid}/>
-                        <FormControl.Feedback type="invalid">
-                            Proszę uzupełnić identyfikator urządzenia pomiarowego
-                        </FormControl.Feedback>
-                    </FormGroup>
-                    <FormGroup className="mt-3">
-                        <FormLabel htmlFor="sensorDescription">Opis czujnika:</FormLabel>
-                        <FormControl id="sensorDescription" type="text" as="textarea" rows={3} placeholder="Opis projektu"/>
-                    </FormGroup>
-                    <FormLabel as="h5" className="mt-3">Położenie czujnika:</FormLabel>
-                    <Row xs={3}>
-                        <FormGroup className="mt-2 mb-3">
-                            <FormLabel htmlFor="longitude">Długość geograficzna:</FormLabel>
-                            <FormControl id="longitude"
-                                         type="number"
-                                         onChange={handleOnChangeLongitude}
-                                         isInvalid={isLongitudeInvalid}
-                                         placeholder="Długość geograficzna"/>
-                            <FormControl.Feedback type="invalid">
-                                Długość geograficzna poza zakresem [-180°, 180°]
-                            </FormControl.Feedback>
-                        </FormGroup>
-                        <FormGroup className="mt-2 mb-3">
-                            <FormLabel htmlFor="latitude">Szerokość
-                                geograficzna:</FormLabel>
-                            <FormControl id="latitude"
-                                         type="number"
-                                         onChange={handleOnChangeLatitude}
-                                         isInvalid={isLatitudeInvalid}
-                                         placeholder="Szerokość geograficzna"/>
-                            <FormControl.Feedback type="invalid">
-                                Długość geograficzna poza zakresem [-90°, 90°]
-                            </FormControl.Feedback>
-                        </FormGroup>
-                        <FormGroup className="mt-2 mb-3">
-                            <FormLabel htmlFor="altitude">Wysokość n.p.m.:</FormLabel>
-                            <FormControl id="altitude" type="number" placeholder="Wysokość"/>
-                        </FormGroup>
-                    </Row>
-                    <MeasurementForm/>
-                    <Button variant="outline-dark" className="ms-2 me-4 rounded-5 float-end">+</Button>
-                    <div className="float-end mt-2"><h5>Dodaj czujnik</h5></div>
-                </Form>
-            </Card.Body>
-        </Card>
+        <Form className="m-1 bg-light-subtle">
+            <FormLabel as="h4" className="m-1">
+                Czujnik #{id + 1}
+                <CloseButton className="float-end" onClick={onRemoveSensorClick}/>
+            </FormLabel>
+            <FormGroup className="mt-3">
+                <FormLabel htmlFor="sensorId">ID urządzenia pomiarowego:</FormLabel>
+                <FormControl required
+                             id="sensorId"
+                             type="text"
+                             placeholder="Identyfikator urządzenia pomiarowego"
+                             onChange={handleOnChangeSensorId}
+                             isInvalid={isSensorIdInvalid}/>
+                <FormControl.Feedback type="invalid">
+                    Proszę uzupełnić identyfikator urządzenia pomiarowego
+                </FormControl.Feedback>
+            </FormGroup>
+            <FormGroup className="mt-3">
+                <FormLabel htmlFor="sensorDescription">Opis czujnika:</FormLabel>
+                <FormControl id="sensorDescription" type="text" as="textarea" rows={3} placeholder="Opis projektu"/>
+            </FormGroup>
+            <FormLabel as="h5" className="mt-3">Położenie czujnika:</FormLabel>
+            <Row xs={3}>
+                <FormGroup className="mt-2 mb-3">
+                    <FormLabel htmlFor="longitude">Długość geograficzna:</FormLabel>
+                    <FormControl id="longitude"
+                                 type="number"
+                                 onChange={handleOnChangeLongitude}
+                                 isInvalid={isLongitudeInvalid}
+                                 placeholder="Długość geograficzna"/>
+                    <FormControl.Feedback type="invalid">
+                        Długość geograficzna poza zakresem [-180°, 180°]
+                    </FormControl.Feedback>
+                </FormGroup>
+                <FormGroup className="mt-2 mb-3">
+                    <FormLabel htmlFor="latitude">Szerokość
+                        geograficzna:</FormLabel>
+                    <FormControl id="latitude"
+                                 type="number"
+                                 onChange={handleOnChangeLatitude}
+                                 isInvalid={isLatitudeInvalid}
+                                 placeholder="Szerokość geograficzna"/>
+                    <FormControl.Feedback type="invalid">
+                        Długość geograficzna poza zakresem [-90°, 90°]
+                    </FormControl.Feedback>
+                </FormGroup>
+                <FormGroup className="mt-2 mb-3">
+                    <FormLabel htmlFor="altitude">Wysokość n.p.m.:</FormLabel>
+                    <FormControl id="altitude" type="number" placeholder="Wysokość"/>
+                </FormGroup>
+            </Row>
+            <MeasurementForm/>
+            <div style={isAddButtonVisible ? {}: {display: "none"}}>
+                <Button variant="outline-dark" className="ms-2 me-4 rounded-5 float-end" onClick={onAddSensorClick}>+</Button>
+                <div className="float-end mt-2"><h5>Dodaj czujnik</h5></div>
+            </div>
+        </Form>
     );
 }
 
