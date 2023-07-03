839927d63e2dd697163c552205826294b60c45f6
mateuszbarnacki <barnackimateusz@gmail.com>
2023-07-03 13:07:48 +0200

    Add delete project modal

--- client/src/components/project/ProjectCard.js
+++ client/src/components/project/ProjectCard.js
@@ -2,26 +2,33 @@ import CardHeader from "react-bootstrap/CardHeader";
 import Card from "react-bootstrap/Card";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 import Button from "react-bootstrap/Button";
+import {Fragment, useState} from "react";
+import DeleteProjectModal from "./modals/DeleteProjectModal";
 
 function ProjectCard({item}) {
+    const [showDeleteProjectModal, setShowDeleteProjectModal] = useState(false);
+    const handleDeleteProjectModalClose = () => {
+        setShowDeleteProjectModal(false);
+    };
+
     return (
-        <Card className="border-black m-3">
-            <CardHeader as="h5">{item.name}</CardHeader>
-            <Card.Body>
-                <Card.Title>Informacje o projekcie</Card.Title>
-                <Card.Text>{item.description}</Card.Text>
-                <ButtonGroup className="mb-2">
-                    <Button variant="outline-dark"
-                            style={{border: "1px solid black"}}>Dodaj pomiary</Button>
-                    <Button variant="outline-dark"
-                            style={{border: "1px solid black"}}>Edytuj projekt</Button>
-                    <Button variant="outline-dark"
-                            style={{border: "1px solid black"}}>Sklonuj</Button>
-                    <Button variant="outline-dark"
-                            style={{border: "1px solid black"}}>Usuń</Button>
-                </ButtonGroup>
-            </Card.Body>
-        </Card>
+        <Fragment>
+            <Card className="border-black m-3">
+                <CardHeader as="h5">{item.name}</CardHeader>
+                <Card.Body className="text-center">
+                    <Card.Title>Informacje o projekcie</Card.Title>
+                    <Card.Text>{item.description}</Card.Text>
+                    <ButtonGroup className="mb-2">
+                        <Button variant="outline-dark" style={{border: "1px solid black"}}>Dodaj pomiar</Button>
+                        <Button variant="outline-dark" style={{border: "1px solid black"}}>Sklonuj</Button>
+                        <Button variant="outline-dark" style={{border: "1px solid black"}}
+                                onClick={() => setShowDeleteProjectModal(true)}>Usuń</Button>
+                    </ButtonGroup>
+                </Card.Body>
+            </Card>
+            <DeleteProjectModal show={showDeleteProjectModal}
+                                closeModal={handleDeleteProjectModalClose}/>
+        </Fragment>
     );
 }
 
---
+++ client/src/components/project/modals/DeleteProjectModal.js
@@ -0,0 +1,39 @@
+import Modal from "react-bootstrap/Modal";
+import ModalHeader from "react-bootstrap/ModalHeader";
+import ModalTitle from "react-bootstrap/ModalTitle";
+import ModalBody from "react-bootstrap/ModalBody";
+import ModalFooter from "react-bootstrap/ModalFooter";
+import Button from "react-bootstrap/Button";
+
+function DeleteProjectModal({show, closeModal}) {
+    return (
+        <Modal show={show} centered dialogClassName="delete-modal">
+            <ModalHeader style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center"
+            }}>
+                <ModalTitle as="h3">
+                    Usuń projekt
+                </ModalTitle>
+            </ModalHeader>
+            <ModalBody className="text-center" as="h5">
+                Czy na pewno chcesz usunąć projekt?
+            </ModalBody>
+            <ModalFooter style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center"
+            }}>
+                <Button variant="danger" onClick={closeModal} size="lg">
+                    Nie
+                </Button>
+                <Button variant="success" onClick={closeModal} size="lg">
+                    Tak
+                </Button>
+            </ModalFooter>
+        </Modal>
+    )
+}
+
+export default DeleteProjectModal;
