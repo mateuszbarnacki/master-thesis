bc016cdd1382c49121abeb84094cda9d4aacfba9
mateuszbarnacki <barnackimateusz@gmail.com>
2023-07-10 12:13:47 +0200

    Mock user repository in jwt factory test

--- auth-service/src/test/java/agh/wfiis/weather/auth/jwt/JwtFactoryTest.java
+++ auth-service/src/test/java/agh/wfiis/weather/auth/jwt/JwtFactoryTest.java
@@ -1,22 +1,29 @@
 package agh.wfiis.weather.auth.jwt;
 
+import agh.wfiis.weather.principal.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.authentication.TestingAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.Optional;
 
 @SpringBootTest
 class JwtFactoryTest {
     @Autowired
     private JwtFactory jwtFactory;
+    @MockBean
+    private UserRepository userRepository;
 
     @Test
     void shouldGenerateToken() {
+        Mockito.when(userRepository.findByUsername("Test")).thenReturn(Optional.empty());
         Authentication testAuthentication = givenTestAuthentication();
-
         String token = whenGenerateToken(testAuthentication);
 
         thenTokenIsNotBlank(token);
