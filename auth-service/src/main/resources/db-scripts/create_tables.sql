93659c6297deb799121f908a40b090c7ea44e46a
mateuszbarnacki <barnackimateusz@gmail.com>
2023-07-14 12:36:10 +0200

    Change password length

--- auth-service/src/main/resources/db-scripts/create_tables.sql
+++ auth-service/src/main/resources/db-scripts/create_tables.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS wfiis.user
     username    VARCHAR(50)  NOT NULL,
     email       VARCHAR(255) NOT NULL,
     description VARCHAR,
-    password    VARCHAR(50)  NOT NULL
+    password    VARCHAR(255)  NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS wfiis.role
