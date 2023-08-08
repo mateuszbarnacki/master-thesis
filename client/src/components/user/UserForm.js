9d6b230bc30389a5235e835fdc5c6619adb71205
mateuszbarnacki <barnackimateusz@gmail.com>
2023-08-08 22:00:16 +0200

    Connect register user form with server

--- client/src/components/user/UserForm.js
+++ client/src/components/user/UserForm.js
@@ -6,11 +6,11 @@ import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
 import {useState} from "react";
 import UserProjectsList from "./UserProjectsList";
+import * as P from "../../api/paths";
 
-function UserForm() {
+function UserForm({handleAlert}) {
     const [isPasswordInvalidRepeated, setIsPasswordInvalidRepeated] = useState(false);
-    const [checkedProjects, updateCheckedProjects] = useState([]);
-
+    const [checkedProjects, setCheckedProjects] = useState([]);
     function handleRepeatedPasswordChange(event) {
         const password = document.getElementById("password").value;
         const repeatedPassword = document.getElementById("repeatedPassword").value;
@@ -20,12 +20,41 @@ function UserForm() {
         }
         setIsPasswordInvalidRepeated(password !== repeatedPassword);
     }
+    const validateEmail = (email) => {
+        return /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(email);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        const repeatedPassword = e.target.repeatedPassword.value;
+        if (password !== repeatedPassword || !validateEmail(email)) {
+            e.stopPropagation();
+            return;
+        }
+        const userDto = {
+            username: e.target.login.value,
+            email: email,
+            description: e.target.information.value,
+            password: repeatedPassword,
+            roles: [],
+            projects: checkedProjects
+        };
+        fetch(P.base + P.users, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(userDto)
+        })
+            .then(data => setCheckedProjects([]))
+            .catch(error => handleAlert(true));
+    };
 
     return (
         <Card>
             <Card.Body>
                 <Card.Text as="h4" className="m-1">Dodaj nowego użytkownika</Card.Text>
-                <Form>
+                <Form onSubmit={handleSubmit}>
                     <FormGroup className="m-3 mt-4">
                         <FormLabel htmlFor="login" className="mb-3" as="h5">Login:</FormLabel>
                         <FormControl type="text" id="login" placeholder="Podaj login" required/>
@@ -43,8 +72,8 @@ function UserForm() {
                     <FormGroup className="m-3 mt-4">
                         <FormLabel className="mb-3" as="h5">Uczestnictwo w projektach:</FormLabel>
                         <UserProjectsList id="user-form-list"
-                                          handleAlert={() => {}}
-                                          updateCheckedProjects={(values) => updateCheckedProjects(values)}/>
+                                          handleAlert={(value) => handleAlert(value)}
+                                          updateCheckedProjects={(values) => setCheckedProjects(values)}/>
                     </FormGroup>
                     <FormGroup className="m-3 mt-4">
                         <FormLabel htmlFor="password" className="mb-3" as="h5">Hasło:</FormLabel>
