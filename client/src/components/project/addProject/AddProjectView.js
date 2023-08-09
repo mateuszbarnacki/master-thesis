91dd7d6d93d55f485e829f9df0d52c3295b19282
mateuszbarnacki <barnackimateusz@gmail.com>
2023-08-09 14:36:35 +0200

    Connect add project action with server

--- client/src/components/project/addProject/AddProjectView.js
+++ client/src/components/project/addProject/AddProjectView.js
@@ -6,11 +6,29 @@ import Menu from "../../Menu";
 import ProjectForm from "./ProjectForm";
 import CardHeader from "react-bootstrap/CardHeader";
 import Footer from "../../Footer";
+import {useNavigate} from "react-router-dom";
+import {validateProject} from "./FormValidator";
+import * as P from "../../../api/paths";
+import Alert from "react-bootstrap/Alert";
+import * as C from "../../../api/constants";
 
 function AddProjectView() {
+    const navigate = useNavigate();
+    const [isAlert, setIsAlert] = useState(false);
+    const [alertMessage, setAlertMessage] = useState('');
     const [ids, setIds] = useState(Array.of(0));
     const [counter, setCounter] = useState(1);
     const [spatialMode, setSpatialMode] = useState("STATIONARY");
+    const alert = (
+        <Alert variant="danger" dismissible
+               onClose={() => {
+                   setIsAlert(false);
+                   setAlertMessage('');
+               }}>
+            <Alert.Heading>Błąd tworzenia projektu</Alert.Heading>
+            Podczas tworzenia projektu wykryto następujące błędy: {alertMessage}
+        </Alert>
+    );
     const handleAddSensorClick = () => {
         const newIds = ids.slice();
         newIds.push(counter);
@@ -56,8 +74,8 @@ function AddProjectView() {
                 const minValue = document.getElementById('minValue-' + i + '-' + j).value;
                 const maxValue = document.getElementById('maxValue-' + i + '-' + j).value;
                 const range = {
-                    min: minValue,
-                    max: maxValue
+                    min: parseFloat(minValue),
+                    max: parseFloat(maxValue)
                 };
                 const errorValue = document.getElementById('errorValue-' + i + '-' + j).value;
                 measurements.push({
@@ -65,19 +83,19 @@ function AddProjectView() {
                     description: measurementDescription,
                     unit: measurementUnit,
                     range: range,
-                    validate: validate,
-                    errorValue: errorValue,
-                    aggregate: aggregate,
+                    validate: (validate === 'true'),
+                    errorValue: parseInt(errorValue),
+                    aggregate: (aggregate === 'true'),
                     aggregationInterval: aggregationInterval,
-                    maxBreak: maxBreak
+                    maxBreak: parseInt(maxBreak)
                 });
                 measurement = document.getElementById('measurement-' + i + "-" + ++j);
             }
             sensors.push({
                 deviceId: sensorId,
-                latitude: latitude,
-                longitude: longitude,
-                altitude: altitude,
+                latitude: parseFloat(latitude),
+                longitude: parseFloat(longitude),
+                altitude: parseFloat(altitude),
                 measurementSchema: {
                     measurements: measurements
                 }
@@ -93,11 +111,40 @@ function AddProjectView() {
             sensors: sensors
         };
         console.log(projectDto);
+        const validationResult = validateProject(projectDto);
+        if (validationResult.length > 0) {
+            const errorMessage = validationResult.join('\n');
+            setAlertMessage(errorMessage);
+            setIsAlert(true);
+            return;
+        }
+        fetch(P.base + P.projects, {
+            method: "POST",
+            headers: {
+                "Authorization": "Bearer " + window.localStorage.getItem(C.localStorageAuthToken),
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(projectDto)
+        })
+            .then(res => {
+                if (res.status === 401) {
+                    navigate('/');
+                } else if (res.status === 201) {
+                    navigate('/list');
+                } else {
+                    return res.json().then(obj => {throw new Error(obj.message)});
+                }
+            })
+            .catch(error => {
+                setAlertMessage(error.message);
+                setIsAlert(true);
+            });
     };
 
     return (
         <Fragment>
             <Menu/>
+            {isAlert ? alert : null}
             <Card className="border-black"
                   style={{
                       marginLeft: "4vw",
--- client/src/components/project/addProject/FormValidator.js
+++ client/src/components/project/addProject/FormValidator.js
@@ -6,16 +6,59 @@ export function isNumberOutOfRange(value, minValue, maxValue) {
     return (value < minValue || value > maxValue);
 }
 
-export function validateAcronym(acronym) {
+export function validateProject(projectDto) {
+    const errors = [];
+    errors.push(validateRequiredString('Nazwa projektu', projectDto.name).toString());
+    errors.push(validateAcronym(projectDto.acronym).toString());
+    errors.push(validateSensors(projectDto.sensors).toString());
+    return errors.filter(item => !isStringNullOrEmpty(item));
+}
+
+function validateSensors(sensors) {
+    const errors = [];
+    for (const sensor of sensors) {
+        errors.push(validateRequiredString('Identyfikator czujnika', sensor.deviceId).toString());
+        errors.push(validateMeasurements(sensor.measurementSchema.measurements).toString());
+    }
+    return errors.filter(item => !isStringNullOrEmpty(item)).join('\n');
+}
+
+function validateMeasurements(measurements) {
+    const errors = [];
+    for (const measurement of measurements) {
+        errors.push(validateRequiredString('Nazwa parametru', measurement.name).toString());
+        errors.push(validateRequiredString('Jednostka', measurement.unit).toString());
+        errors.push(validateRequiredNumber('Wartość błędu', measurement.errorValue).toString());
+    }
+    return errors.filter(item => !isStringNullOrEmpty(item)).join('\n');
+}
+
+function validateRequiredString(name, text) {
+    const errors = [];
+    if (isStringNullOrEmpty(text)) {
+        errors.push(name + ' jest pusty');
+    }
+    return errors.filter(item => !isStringNullOrEmpty(item)).join('\n');
+}
+
+function validateRequiredNumber(name, number) {
+    const errors = [];
+    if (isNaN(parseFloat(number))) {
+        errors.push(name + ' nie jest liczbą');
+    }
+    return errors.filter(item => !isStringNullOrEmpty(item)).join('\n');
+}
+
+function validateAcronym(acronym) {
     const errors = [];
     if (acronym.length > 10) {
-        errors.push('Akronim ma więcej niż 10 znaków!');
+        errors.push('Akronim ma więcej niż 10 znaków');
     }
     if (!/^([a-zA-Z0-9]|-)+$/.test(acronym)) {
-        errors.push('Akronim zawiera nieodpowiedni znak!');
+        errors.push('Akronim zawiera nieodpowiedni znak');
     }
     if (/^\d$/.test(acronym[0])) {
-        errors.push('Pierwszy znak akronimu nie może być cyfrą!');
+        errors.push('Pierwszy znak akronimu nie może być cyfrą');
     }
-    return errors;
+    return errors.filter(item => !isStringNullOrEmpty(item)).join('\n');
 }
